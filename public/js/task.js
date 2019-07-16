2400220a28f30a86a0e6a99c7d875c87988bf2d4
SMaxKloeppel <github@smaxx.com>
2019-07-16 09:08:02 -0600

    Fix delete - w/icon

--- public/js/task.js
+++ public/js/task.js
@@ -2,7 +2,7 @@ const tasks = document.getElementById('tasklist');
 
 if (tasklist) {
   tasklist.addEventListener('click', e => {
-    if (e.target.className === 'delete-task') {
+    if (e.target.className === 'material-icons') {
       if (confirm('Are you sure?')) {
         const id = e.target.getAttribute('data-id');
 
